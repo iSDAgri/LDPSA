bfb6bfc0e1d9a50aef3563b6ed2997df2d831ca4
mgwalsh <markusgwalsh@gmail.com>
2014-10-02 18:40:10 +0300

    changed to REML models w lab covariates

--- PSD_differences.R
+++ PSD_differences.R
@@ -1,4 +1,4 @@
-# run the LDPSA_starter.R first
+# run the LDPSA_starter.R script first
 # M. Walsh, October 2014
 
 # Load lab covariates -----------------------------------------------------
@@ -7,35 +7,23 @@ download("https://www.dropbox.com/s/gcga9uyt2b8cv9h/Lab_cov.csv.zip?dl=0", "Lab_
 unzip("Lab_cov.csv.zip", overwrite=T)
 labcov <- read.table("Lab_cov.csv", header=T, sep=",")
 
-# Data setup --------------------------------------------------------------
+# REML analyses ---------------------------------------------------
 
 # Ultrasonic treatment differences of samples dispersed in water
-w1  <- subset(ldps.comp, TRT=="w1", select=c(Site, PID, SSN, Depth, V1, V2))
-w4  <- subset(ldps.comp, TRT=="w4", select=c(SSN, V1, V2))
-colnames(w1) <- c("Site", "PID", "SSN", "Depth", "w1v1", "w1v2")
-colnames(w4) <- c("SSN", "w4v1", "w4v2")
-water <- merge(w1, w4, by="SSN")
-attach(water)
-water$dv1 <- w4v1-w1v1
-water$dv2 <- w4v2-w1v2
-water$dw4 <- (w4v1-w1v1)+(w4v2-w1v2)
-detach(water)
-psdw <- merge(water, labcov, by="SSN")
+water <- subset(ldps.comp, Disp=="water", select=c(Site, GID, SSN, Ultra, Depth, V1, V2))
+water <- merge(water, labcov, by="SSN")
 
-# Ultrasonic treatment differences of samples dispersed in Calgon
-c1  <- subset(ldps.comp, TRT=="c1", select=c(Site, PID, SSN, Depth, V1, V2))
-c4  <- subset(ldps.comp, TRT=="c4", select=c(SSN, V1, V2))
-colnames(c1) <- c("Site", "PID", "SSN", "Depth", "c1v1", "c1v2")
-colnames(c4) <- c("SSN", "c4v1", "c4v2")
-calgon <- merge(c1, c4, by="SSN")
-attach(calgon)
-calgon$dv1 <- c4v1-c1v1
-calgon$dv2 <- c4v2-c1v2
-calgon$dc4 <- (c4v1-c1v1)+(c4v2-c1v2)
-detach(calgon)
-psdc <- merge(calgon, labcov, by="SSN")
+wV1.lmer <- lmer(V1~I(Depth/100)+Ultra*log(SOC)+(1|Site)+(1|GID:Site), data=water)
+summary(wV1.lmer)
+wV2.lmer <- lmer(V2~I(Depth/100)+Ultra*log(SOC)+(1|Site)+(1|GID:Site), data=water)
+summary(wV2.lmer)
 
+# Ultrasonic treatment differences of samples dispersed in calgon
+calgon <- subset(ldps.comp, Disp=="calgon", select=c(Site, GID, SSN, Ultra, Depth, V1, V2))
+calgon <- merge(calgon, labcov, by="SSN")
 
-
-
+cV1.lmer <- lmer(V1~I(Depth/100)+Ultra*log(SOC)+(1|Site)+(1|GID:Site), data=calgon)
+summary(cV1.lmer)
+cV2.lmer <- lmer(V2~I(Depth/100)+Ultra*log(SOC)+(1|Site)+(1|GID:Site), data=calgon)
+summary(cV2.lmer)
 
